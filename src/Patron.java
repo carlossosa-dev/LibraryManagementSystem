8d811ae8a26e744201c2fce080e0c9f60d99e96e
carlossosa-dev <carlossosadesign@outlook.com>
2025-01-26 18:35:40 -0500

    Add files via upload

---
+++ src/Patron.java
@@ -0,0 +1,52 @@
+public class Patron {
+    private String id;
+    private String name;
+    private String address;
+    private double overdueFine;
+
+    // Constructor
+    public Patron(String id, String name, String address, double overdueFine) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.overdueFine = overdueFine;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getOverdueFine() {
+        return overdueFine;
+    }
+
+    public void setOverdueFine(double overdueFine) {
+        this.overdueFine = overdueFine;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Address: " + address + ", Fine: $" + overdueFine;
+    }
+}
